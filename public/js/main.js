19c50117f7df42c69381b1ad73214ea2d57eee97
abhijeet <abhijeetgoel77@gmail.com>
2013-08-31 12:49:55 +0530

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -85,6 +85,49 @@
 	   {
 			FB.ui({
 			  method: 'send',
-			  link: 'www.fundooz.in',
+			  link: 'www.fundooz.in'
 			});
-	   }		   
+	   }
+	   var socket = io.connect('http://localhost:3000');
+	   socket.on('connect',function(){
+			socket.emit('adduser',prompt("What's your name?"));
+	   });
+	   socket.on('updatechat',function(username,data){
+			$("#conversation").append('<b>'+username + ':</b>' + data + '<br>');
+	   });
+	   socket.on('updaterooms',function(rooms,current_room){
+			$('#rooms').empty();
+			$.each(rooms,function(key,value){
+				if(value == current_room)
+				{
+					$('#rooms').append('<div>'+value+'</div>');
+				}
+				else
+				{
+					$('#rooms').append('<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>');
+				}
+			});
+	   });
+	   function switchRoom(room){
+			socket.emit('switchRoom',room);
+	   }   
+	   
+	   socket.on('updateusers',function(data){
+			$('#users').empty();
+			$.each(data,function(key,value){
+				$('#users').append('<div>' + key + '</div>');
+			});
+	   });
+	   $(function(){
+			$('#datasend').click(function(){
+				var message = $('#data').val();
+				$('#data').val('');
+				socket.emit('sendchat',message);
+			});
+			$('#data').keypress(function(e){
+				if(e.which == 13){
+					$(this).blur();
+					$('#datasend').focus().click();
+				}
+			});
+	   });
--- web.js
+++ web.js
@@ -12,6 +12,39 @@ var app = express.createServer(
   
 );
 
+var io = require('socket.io').listen(app);
+
+var usernames = {};
+var rooms = ['room1','room2','room3'];
+io.sockets.on('connection',function(socket){
+	socket.on('sendchat',function(data){
+		io.sockets.in(socket.room).emit('updatechat',socket.username,data);
+	});
+	socket.on('adduser',function(username){
+		socket.username = username;
+		socket.room = 'room1';
+		usernames[username] = username;
+		socket.join('room1');
+		socket.emit('updatechat','SERVER','you have connected to room1');
+		socket.broadcast.to('room1').emit('updatechat','SERVER',username + 'has connected to this room');
+		socket.emit('updaterooms',rooms,'room1');
+	});
+	socket.on('switchRoom',function(newroom){
+		socket.leave(socket.room);
+		socket.join(newroom);
+		socket.emit('updatechat','SERVER','you have connected to ' + newroom);
+		socket.broadcast.to(socket.room).emit('updatechat','SERVER',socket.username + 'has left the room');
+		socket.room = newroom;
+		socket.broadcast.to(newroom).emit('updatechat','SERVER',socket.username + 'has joined the room');
+		socket.emit('updaterooms',rooms,newroom);
+	});
+	socket.on('disconnect',function(){
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers',usernames);
+		socket.broadcast.emit('updatechat','SERVER',socket.username + 'has disconnected');
+		socket.leave(socket.room);
+	});
+});
 // listen to the PORT given to us in the environment
 var port = process.env.PORT || 3000;
 
